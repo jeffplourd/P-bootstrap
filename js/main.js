afdcdf66006a4ce1f0818fa3cf73244840076387
jplourd <jeffplourd@gmail.com>
2015-01-20 13:26:56 -0800

    This is the first entry for my bootstrap framework redo and canvas addition.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('document').ready(function() {
+  var c = document.getElementById("canvas");
+  var ctx=c.getContext("2d");
+
+
+make_base();
+
+function make_base()
+{
+  base_image = new Image();
+  base_image.src = 'abrogate.jpg';
+  base_image.onload = function(){
+    ctx.drawImage(base_image, 0, 0);
+  }
+}
+
+
+canvas.onclick = function(){
+  ctx.beginPath();
+  ctx.arc(95,50,40,0,2*Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = "blue";
+// Start at (0,0) and draw a 50px x 50px blue rectangle.
+  ctx.fillRect(0,0,50,50);
+// Start at (0,0) and clear a 25px x 25px rectangle.
+  ctx.clearRect(0,0,25,25);
+}
+});
